d93fcf18916c5810e2dc486562bbb3027295ed60
Roman Jasko <david.eyett@gmail.com>
2017-06-26 16:09:31 +0100

    anotherOne function

--- practice.js
+++ practice.js
@@ -1,16 +1,16 @@
+function onGetCaptureValue(formMappingId, value, controls) {
+    if (formMappingId === 418506) {
 
+        value = (function(dl) {
+            for (var i = 0; i < dl.length; i++)
+                if (dl[i]['user.profile.country'])
+                    return dl[i]['user.profile.country'];
+        })(dataLayer);
+        console.log("CHANGE 1");
+    }
+    return value;
+}
 
-
-
-function onGetCaptureValue (formMappingId, value, controls) {
-  if(formMappingId===418506){
-
-   value = (function(dl){
-     for(var i=0;i<dl.length;i++)
-     if(dl[i]['user.profile.country'])
-     return dl[i]['user.profile.country'];
-   })(dataLayer);
-    console.log("CHANGE 1");
-  }
-  return value;
+function anotherOne {
+    console.log('this is another function');
 }
